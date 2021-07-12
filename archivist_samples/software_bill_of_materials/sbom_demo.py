91ec13475998df1f3555cf53d504c44e0155fde6
JAG-UK <jon.geater@gmail.com>
2021-07-12 15:53:24 +0100

    !!FIXUP cleaan formatting

--- archivist_samples/software_bill_of_materials/sbom_demo.py
+++ archivist_samples/software_bill_of_materials/sbom_demo.py
@@ -20,47 +20,49 @@ from archivist import archivist
 from software_package import SoftwarePackage
 #from software_deployment import SoftwareDeployment
 
+
 def main():
-  # Refer to RKVST documentation for how to get an authtoken
-  try:
-    with open(".auth_token", mode="r") as tokenfile:
-      authtoken = tokenfile.read().strip()
-  except FileNotFoundError:
-    exit("ERROR: Auth token not found. Please store your bearer token in a file called '.auth_token' and try again.")
+    # Refer to RKVST documentation for how to get an authtoken
+    try:
+        with open(".auth_token", mode="r") as tokenfile:
+            authtoken = tokenfile.read().strip()
+    except FileNotFoundError:
+        exit("ERROR: Auth token not found. Please store your bearer token in a file called '.auth_token' and try again.")
 
-  # Initialize connection to Archivist
-  arch = archivist.Archivist(
-    "https://rkvst.poc.jitsuin.io",
-    auth=authtoken,
-  )
+    # Initialize connection to Archivist
+    arch = archivist.Archivist(
+        "https://rkvst.poc.jitsuin.io",
+        auth=authtoken,
+    )
 
-  # SoftwarePackage class encapsulates SBOM object in RKVST 
-  print("Creating Software Package Asset...", end="")
-  package = SoftwarePackage(arch)
+    # SoftwarePackage class encapsulates SBOM object in RKVST
+    print("Creating Software Package Asset...", end="")
+    package = SoftwarePackage(arch)
 
-  package.create(
-    "ACME Roadrunner Detector 2013 Coyote Edition SP1",
-    "Different box, same great taste!",
-    attachments=["attachments/Comp_2.jpeg"]
-  ) 
-  print(f"Software Package Created (Identity={package.asset['identity']})")
+    package.create(
+        "ACME Roadrunner Detector 2013 Coyote Edition SP1",
+        "Different box, same great taste!",
+        attachments=["attachments/Comp_2.jpeg"]
+    )
+    print(f"Software Package Created (Identity={package.asset['identity']})")
 
-  # Make a release
-  print("Making a release...", end="")
-  package.release({
-    "name": "ACME Roadrunner Detector 2013 Coyote Edition SP1",
-    "description": "v4.1.5 Release - ACME Roadrunner Detector 2013 Coyote Edition SP1",
-    "hash": "a314fc2dc663ae7a6b6bc6787594057396e6b3f569cd50fd5ddb4d1bbafd2b6a",
-    "version": "v4.1.5",
-    "author": "The ACME Corporation",
-    "supplier": "Coyote Services, Inc.",
-    "uuid": "com.acme.rrd2013-ce-sp1-v4-1-5-0"},
-    attachments=["attachments/v4_1_5_sbom.xml"],
-    custom_attrs={"sbom_license": "www.gnu.org/licenses/gpl.txt"
-  })
-  print("Release registered.")
-
+    # Make a release
+    print("Making a release...", end="")
+    package.release(
+        {
+            "name": "ACME Roadrunner Detector 2013 Coyote Edition SP1",
+            "description": "v4.1.5 Release - ACME Roadrunner Detector 2013 Coyote Edition SP1",
+            "hash": "a314fc2dc663ae7a6b6bc6787594057396e6b3f569cd50fd5ddb4d1bbafd2b6a",
+            "version": "v4.1.5",
+            "author": "The ACME Corporation",
+            "supplier": "Coyote Services, Inc.",
+            "uuid": "com.acme.rrd2013-ce-sp1-v4-1-5-0"
+        },
+        attachments=["attachments/v4_1_5_sbom.xml"],
+        custom_attrs={"sbom_license": "www.gnu.org/licenses/gpl.txt"}
+    )
+    print("Release registered.")
 
 
 if __name__ == "__main__":
-   main()
+    main()
