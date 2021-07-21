313202660caad8d8e7fc8d577a2d6675ec118367
Madia Wilder <mwilder@Madias-MBP.attlocal.net>
2021-07-21 16:40:42 -0500

    Partial Draft of Nuclear Demo

---
+++ archivist_samples/nuclear/nuclear.py
@@ -0,0 +1,170 @@
+# pylint:disable=missing-function-docstring      # docstrings
+# pylint:disable=missing-module-docstring      # docstrings
+# pylint:disable=missing-class-docstring      # docstrings
+from operator import floordiv
+from typing import Optional
+from random import random
+
+# pylint:disable=unused-import      # To prevent cyclical import errors forward referencing is used
+# pylint:disable=cyclic-import      # but pylint doesn't understand this feature
+
+from archivist import archivist as type_helper
+
+
+class Nuclear:
+    def __init__(self, arch: "type_helper.Archivist"):
+        self._arch = arch
+        self._asset = None
+        # self._attachments = None
+
+    @property
+    def arch(self):
+        return self._arch
+
+    @property
+    def asset(self):
+        return self._asset
+
+    # @property
+    # def attachements(self):
+    #    return self._attachments
+
+    # Asset Creation
+    def create(
+        self,
+        nw_name: str,
+        nw_description: str,
+        nw_code: str,
+        nw_lifecycle,
+        *,
+        attachments: Optional[list] = None,
+        custom_attrs: Optional[dict] = None,
+    ):
+
+        # self._add_attachments(attachments)
+
+        attrs = {
+            "arc_display_name": nw_name,
+            "arc_description": nw_description,
+            "arc_display_type": "Nuclear Waste Item",
+            "nw_waste_code": nw_code,
+            "nw_lifecycle_stage": nw_lifecycle,
+            "nw_namespace": random(),
+            "attachments": attachments or [],
+           # "arc_attachments": [
+           #     {
+           #         "arc_display_name": "arc_primary_image",
+           #         "arc_attachment_identiy": attachment["identity"],
+           #         "arc_hash_value": attachment["hash"]["value"],
+           #         "arc_hash_alg": attachment["hash"]["alg"],
+           #    }
+           #     for attachment in self._attachments //
+           # ],
+        }
+        if custom_attrs is not None:
+            attrs.update(custom_attrs)
+
+        behaviours = [
+            "Attachments",
+            "Firmware",
+            "LocationUpdate",
+            "Maintenance",
+            "RecordEvidence",
+        ]
+        self._asset = self.arch.assets.create(behaviours, attrs, confirm=True)
+        return self._asset
+
+    # Asset load by unique identity
+    def read(
+        self,
+        identity: str,
+    ):
+        self._asset = self.arch.assets.read(identity)
+
+    # Asset load by attribute(s)
+    def read_by_signature(
+        self,
+        attributes: Optional[dict],
+    ):
+        # Hard-wire the Asset type
+        newattrs = attributes.copy()
+        newattrs["arc_display_type"] = "Nuclear Waste Item"
+
+        # Note: underlying Archivist will raise ArchivistNotFoundError or
+        # ArchivistDuplicateError unless this set of attributes points to
+        # a single unique asset
+        self._asset = self.arch.assets.read_by_signature(attrs=newattrs)
+
+    # Characterize Events
+    def characterize(
+        self,
+        nw: dict,
+        *,
+        attachments: Optional[list] = None,
+        latest_nw: Optional[dict] = None,
+        custom_attrs: Optional[dict] = None,
+        custom_asset_attrs: Optional[dict] = None,
+    ):
+        # self._add_attachments(attachments)
+        props = {
+            "operation": "Record",
+            "behaviour": "RecordEvidence",
+        }
+
+        if latest_nw is None:
+            latest_nw = nw
+        attrs = {
+            "arc_description": nw["description"],
+            "arc_evidence": "Characterize",
+            "arc_display_type": "Characterize",
+            "arc_attachments": attachments or [],
+            #"arc_attachments": [
+            #    {
+            #        "arc_display_name": nw["description"],
+            #        "arc_attachment_identity": attachment["identity"],
+            #        "arc_hash_value": attachment["hash"]["value"],
+            #        "arc_hash_alg": attachment["hash"]["alg"],
+            #    }
+            #    for attachment in self._attachments
+            #],
+        }
+        if custom_attrs is not None:
+            atrrs.update(custom_attrs)
+
+        asset_attrs = {
+            "arc_display_name":latest_nw["name"],
+            "nw_item_activity_group_b1": latest_nw["nw_item_activity_group_b1"],
+            "nw_fissile_particles": latest_nw["nw_fissile_particles"],
+            "nw_active_particles": latest_nw["nw_active_particles"],
+            "nw_activity_nonalpha": latest_nw["nw_activity_nonalpha"],
+            "nw_item_activity_group_a": latest_nw["nw_item_activity_group_a"],
+            "nw_explosives": latest_nw["nw_explosives"],
+            "nw_soluble_solids": latest_nw["nw_soluble_solids"],
+            "nw_oxidizing_agents": latest_nw["nw_oxidizing_agents"],
+            "nw_item_activity_group_b2": latest_nw["nw_item_activity_group_b2"],
+            "nw_item_activity_group_c": latest_nw["nw_item_activity_group_c"],
+            "nw_waste_weight": latest_nw["nw_waste_weight"],
+            "nw_activity_alpha": latest_nw["nw_activity_alpha"],
+            "nw_waste_code": latest_nw["nw_waste_code"],
+            "nw_lifecycle_stage": latest_nw["nw_lifecycle_stage"],
+            "nw_target_stream": latest_nw["nw_target_stream"],
+            "nw_free_liquid": latest_nw["nw_free_liquid"],    
+        }
+        if custom_asset_attrs is not None:
+            asset_atrs.update(custom_asset_attrs)
+
+        return self.arch.events.create(
+            self.asset["identity"],
+            props=props,
+            attrs=attrs,
+            asset_attrs=asset_atrs,
+            confirm=True,
+        )
+
+    # def _add_attachments(self, attachements: list):
+    #    self._attachments = []
+    #    for attachment in attachments:
+    #        with open(f"{attachment}", "rb") as fd:
+    #            self._attachments.append(self.arch.attachements.upload(fd))
+
+        
---
+++ archivist_samples/nuclear/run.py
@@ -0,0 +1,89 @@
+#   Copyright 2021 Jitsuin, inc
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+#   This is API SAMPLE CODE, not for production use.
+
+# pylint:  disable=missing-docstring
+
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 'importlib_resources'.
+    import importlib_resources as pkg_resources
+
+import logging
+
+from archivist import about
+
+#from . import nuclear_files
+
+from nuclear import Nuclear
+from nuclear_files import assets as image_assets
+
+
+LOGGER = logging.getLogger(__name__)
+
+def upload_attachment(arch, path, name):
+    with pkg_resources.open_binary(image_assets, "wasteimage.jpeg") as fd:
+        blob = arch.attachments.upload(fd)
+        attachment = {
+            "arc_display_name": name,
+            "arc_attachment_identity": blob["identity"],
+            "arc_hash_value": blob["hash"]["value"],
+            "arc_hash_alg": blob["hash"]["alg"],
+        }
+        return attachment
+
+def run(arch):
+
+    LOGGER.info("Using version %s of jitsuin-archivist", about.__version__)
+    LOGGER.info("Fetching use case test assets namespace %s", arch.namespace)
+
+    # Nuclear class encapsulates SBOM object in RKVST
+    LOGGER.info("Creating Nuclear Asset...")
+    item = Nuclear(arch)
+
+    item.create(
+        "Item-" + random(), "Waste Item demo creation",
+        "0","pre-treatement",
+        attachments=[upload_attachment(arch, "wasteimage.jpeg", "arc_primary_image")],
+    )
+    LOGGER.info("Nuclear Item Asset Created (Identity=%s)", item.asset["identity"])
+
+    # Characterize
+    LOGGER.info("Adding characterization...")
+    item.characterize(
+       {
+           "name": "Item-" + random(),
+            "description": "Charactized",
+            "nw_item_activity_group_b1": "7",
+            "nw_fissile_particles": "20",
+            "nw_active_particles": "0", 
+            "nw_activity_nonalpha": "3",
+            "nw_item_activity_group_a": "0.6",
+            "nw_explosives": "0",
+            "nw_soluble_solids": "10000",
+            "nw_oxidizing_agents": "0",
+            "nw_item_activity_group_b2": "124",
+            "nw_item_activity_group_c": "766",
+            "nw_waste_weight": "32000000",
+            "nw_activity_alpha": "1",
+            "nw_waste_code": "20234",
+            "nw_lifecycle_stage": "characterized",
+            "nw_target_stream": "Combustible",
+            "nw_free_liquid": "0",
+       }
+    )
+    LOGGER.info("Characterization registered...")
+
